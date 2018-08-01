ea767adcae779dcc45e89bd3814b2ded8a996e24
Dayanand B Menashi <dayamenashi@gmail.com>
2018-07-31 20:05:05 -0400

    Add files via upload

---
+++ 052-json-file.js
@@ -0,0 +1,26 @@
+myFunction([
+{
+"display": "Dollar Tree",
+"url": "https://quotes.wsj.com/DLTR"
+},
+{
+"display": "Facebook",
+"url": "https://quotes.wsj.com/FB"
+},
+{
+"display": "Apple",
+"url": "https://quotes.wsj.com/AAPL"
+},
+{
+"display": "Amazon",
+"url": "https://quotes.wsj.com/AMZN"
+},
+{
+"display": "Google",
+"url": "https://quotes.wsj.com/GOOG"
+},
+{
+"display": "Microsoft",
+"url": "https://quotes.wsj.com/MSFT"
+}
+])
