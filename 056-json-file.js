526cef8c40cf52187d4ef7f8583a766dbbba1a48
Dayanand B Menashi <dayamenashi@gmail.com>
2018-08-23 08:14:36 -0400

    Add files via upload

---
+++ 056-json-file.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+var myList = 
+
+  [{
+    "ITEM": "Air Freshner",
+    "PRICE": "$7.00",
+    "DISCOUNT": "5%"
+  }, {
+    "ITEM": "Washing Detergent",
+    "PRICE": "$16.00",
+    "DISCOUNT": "7%"
+  }, {
+    "ITEM": "Hair Gel",
+    "PRICE": "$4.00",
+    "DISCOUNT": "8%"
+  }, {
+    "ITEM": "Cooking Oil",
+    "PRICE": "$12.00",
+    "DISCOUNT": "10%"
+  }]
+  
+   buildHtmlTable(myList);
+  check();
+})
+  
