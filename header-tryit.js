2c38b6c9e428cf9fe99cffd3e46d5c23cb451654
Dayanand B Menashi <dayamenashi@gmail.com>
2018-09-15 14:21:36 -0400

    Add files via upload

--- header-tryit.js
+++ header-tryit.js
@@ -1,6 +1,7 @@
 document.write('<div class="top">'
         + '<a class="w3schools-logo" href="index.html">&nbsp;FlatHTML<span class="dotcom">.com</span></a>'
         + '<div  class="w3-right  toptext "><span class="w3-wide"><b>References :</b></span>'
+		+ '&nbsp;<a href="https://github.com/flathtml/800Examples"target=_blank">Source Code</a>'
 		+ '&nbsp;<a href="https://htmlcolorcodes.com/"target=_blank">HTML Colors</a>'
 		+ '&nbsp;<a href="https://www.w3schools.com/w3css/w3css_examples.asp"target=_blank">W3 CSS</a>'
 		+ '&nbsp;&nbsp;'
